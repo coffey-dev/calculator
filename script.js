d52c827f9e69898dadae8d3f1e3f63530e49a808
coffey-dev <scoffey@abc.gob.ar>
2022-09-26 18:01:41 -0300

    Imprime numeros y los agrega a un array

---
+++ script.js
@@ -0,0 +1,61 @@
+const display = document.getElementById("result");
+let contenedor = [];
+display.textContent = contenedor;
+
+document.getElementById("ac").addEventListener("click", function() {
+    contenedor = [];
+    display.textContent = contenedor;
+   
+  });
+ 
+  document.getElementById("0").addEventListener("click", function() {
+    contenedor.push(0);
+    display.textContent = contenedor.join("");
+   
+  });
+   
+document.getElementById("1").addEventListener("click", function() {
+    contenedor.push(1);
+    display.textContent = contenedor.join("");
+   
+  });
+  document.getElementById("2").addEventListener("click", function() {
+    contenedor.push(2);
+    display.textContent = contenedor.join("");
+   
+  });
+  document.getElementById("3").addEventListener("click", function() {
+    contenedor.push(3);
+    display.textContent = contenedor.join("");
+   
+  });
+  document.getElementById("4").addEventListener("click", function() {
+    contenedor.push(4);
+    display.textContent = contenedor.join("");
+   
+  });
+  document.getElementById("5").addEventListener("click", function() {
+    contenedor.push(5);
+    display.textContent = contenedor.join("");
+   
+  });
+  document.getElementById("6").addEventListener("click", function() {
+    contenedor.push(6);
+    display.textContent = contenedor.join("");
+   
+  });
+  document.getElementById("7").addEventListener("click", function() {
+    contenedor.push(7);
+    display.textContent = contenedor.join("");
+   
+  });
+  document.getElementById("8").addEventListener("click", function() {
+    contenedor.push(8);
+    display.textContent = contenedor.join("");
+   
+  });
+  document.getElementById("9").addEventListener("click", function() {
+    contenedor.push(9);
+    display.textContent = contenedor.join("");
+   
+  });
